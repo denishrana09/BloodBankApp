f8b8e9dcb6fffe2d52497248ded5499814efec11
nitrogen9 <rdxgalaxy7@gmail.com>
2018-03-08 10:58:49 +0530

    many methods added but still Location is null,gps problem

--- app/src/main/java/com/example/denish/bloodbank/SelectGroup.java
+++ app/src/main/java/com/example/denish/bloodbank/SelectGroup.java
@@ -1,10 +1,14 @@
 package com.example.denish.bloodbank;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
@@ -20,10 +24,17 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStates;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -32,11 +43,16 @@ import com.google.firebase.FirebaseApp;
 
 import static com.example.denish.bloodbank.MainActivity.RC_TYPE;
 
-public class SelectGroup extends AppCompatActivity{
+public class SelectGroup extends AppCompatActivity
+        implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
 
     private static final String FINE_LOCATION = android.Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COARSE_LOCATION = android.Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    protected static final int REQUEST_CHECK_SETTINGS = 0x1;
+    private GoogleApiClient mGoogleApiClient;
 
     private Boolean mLocationPermissionGranted = false;
     private FusedLocationProviderClient mFusedLocationProviderClient;
@@ -44,15 +60,22 @@ public class SelectGroup extends AppCompatActivity{
 //    private GoogleApiClient mGoogleApiClient;
 
     private static final String TAG = "SelectGroup";
-    EditText mSelectGroup,mMobileno;
+    EditText mSelectGroup, mMobileno;
     Button mNext;
     String type;
-    
+    double longitude,latitude;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d(TAG, "onCreate: starts");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_group);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
 
         mSelectGroup = findViewById(R.id.et_select_group);
         mMobileno = findViewById(R.id.et_mobileno);
@@ -69,26 +92,26 @@ public class SelectGroup extends AppCompatActivity{
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "Inside SelectGroup Listener");
-                if((type = mSelectGroup.getText().toString()).length() > 0 &&
+                if ((type = mSelectGroup.getText().toString()).length() > 0 &&
                         type.equals("O+") || type.equals("O-") || type.equals("A-") || type.equals("A+")
-                        || type.equals("B+") || type.equals("B-") || type.equals("AB-") || type.equals("AB+")){
+                        || type.equals("B+") || type.equals("B-") || type.equals("AB-") || type.equals("AB+")) {
 
                     SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0); // 0 - for private mode
                     SharedPreferences.Editor editor = pref.edit();
 
                     editor.putString("group", type); // Storing string
-                    editor.putString("mobile",mMobileno.getText().toString());
+                    editor.putString("mobile", mMobileno.getText().toString());
                     editor.apply();
                     Log.d(TAG, "onClick: preference added (group,mobile) :" + type + "," + mMobileno.getText().toString());
 
-                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
 //                    intent.putExtra("group","O+ve");
 //                    intent.putExtra("group",type);
 //                    intent.putExtra("mobile","9988998877");
 //                    intent.putExtra("mobile",mMobileno.getText().toString());
 //                    setResult(RC_TYPE,intent);
                     startActivity(intent);
-                }else{
+                } else {
                     Toast.makeText(SelectGroup.this, "Write Proper Blood Group Please", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -96,7 +119,7 @@ public class SelectGroup extends AppCompatActivity{
     }
 
     //calling getDeviceLocation()
-    private void init(){
+    private void init() {
         Log.d(TAG, "init: starts");
         if (mLocationPermissionGranted) {
             Log.d(TAG, "init: calling getDeviceLocation");
@@ -107,10 +130,7 @@ public class SelectGroup extends AppCompatActivity{
                 Log.d(TAG, "init: returning");
                 return;
             }
-
             getDeviceLocation();
-//            mMap.setMyLocationEnabled(true);
-//            mMap.getUiSettings().setMyLocationButtonEnabled(false);
         }
     }
 
@@ -142,6 +162,21 @@ public class SelectGroup extends AppCompatActivity{
                                 editor.apply();
                                 Log.d(TAG, "onComplete: lat,lon : " + lat + "," + lon);
                             }
+                            else {
+                                Log.d(TAG, "onComplete: inside else where currentlocation is null");
+                                settingsrequest();
+                                //getDeviceLocation();
+//                                Location currentLocation1 = (Location) task.getResult();
+//                                Log.d(TAG, "onComplete: after settingsrequest");
+//                                double lat = currentLocation1.getLatitude();
+//                                double lon = currentLocation1.getLongitude();
+//
+//                                SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0); // 0 - for private mode
+//                                SharedPreferences.Editor editor = pref.edit();
+//                                editor.putString("lat", lat + "");
+//                                editor.putString("lon", lon + "");
+//                                editor.apply();
+                            }
                         }
                     }
                 });
@@ -156,6 +191,7 @@ public class SelectGroup extends AppCompatActivity{
     private void getLocationPermission(){
         String permissions[] = {android.Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
 
+        Log.d(TAG, "getLocationPermission: before if condition");
         if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
                 FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
             if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
@@ -167,11 +203,13 @@ public class SelectGroup extends AppCompatActivity{
                 ActivityCompat.requestPermissions(this,
                         permissions,
                         LOCATION_PERMISSION_REQUEST_CODE);
+                Log.d(TAG, "getLocationPermission: else part 1");
             }
         }else {
             ActivityCompat.requestPermissions(this,
                     permissions,
                     LOCATION_PERMISSION_REQUEST_CODE);
+            Log.d(TAG, "getLocationPermission: else part 2");
         }
     }
 
@@ -197,5 +235,93 @@ public class SelectGroup extends AppCompatActivity{
                 }
             }
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+// Check for the integer request code originally supplied to startResolutionForResult().
+            case REQUEST_CHECK_SETTINGS:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        //startLocationUpdates();
+                        Log.d(TAG, "onActivityResult: Result Ok");
+                        Toast.makeText(this, "Request : Result Ok", Toast.LENGTH_SHORT).show();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        //settingsrequest();//keep asking if imp or do whatever
+                        Log.d(TAG, "onActivityResult: Result cancelled");
+                        Toast.makeText(this, "Request : Result cancelled", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                break;
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        settingsrequest();
+    }
+
+    public void settingsrequest()
+    {
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(30 * 1000);
+        locationRequest.setFastestInterval(5 * 1000);
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true); //this is the key ingredient
+
+        PendingResult<LocationSettingsResult> result =
+                LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient, builder.build());
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                final LocationSettingsStates state = result.getLocationSettingsStates();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        // All location settings are satisfied. The client can initialize location
+                        // requests here.
+                        getDeviceLocation();
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        // Location settings are not satisfied. But could be fixed by showing the user
+                        // a dialog.
+                        try {
+                            // Show the dialog by calling startResolutionForResult(),
+                            // and check the result in onActivityResult().
+                            status.startResolutionForResult(SelectGroup.this, REQUEST_CHECK_SETTINGS);
+                            getDeviceLocation();
+                        } catch (IntentSender.SendIntentException e) {
+                            // Ignore the error.
+                            Toast.makeText(SelectGroup.this, "Error Occured", Toast.LENGTH_SHORT).show();
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        // Location settings are not satisfied. However, we have no way to fix the
+                        // settings so we won't show the dialog.
+                        break;
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d(TAG, "onConnected: called");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "onConnectionSuspended: called");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed: called");
     }
 }
