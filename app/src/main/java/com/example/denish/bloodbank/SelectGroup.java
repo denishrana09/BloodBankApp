a7c3a4ce2f0c9c394d18a3b683a9d9ef78135473
nitrogen9 <rdxgalaxy7@gmail.com>
2018-03-08 12:12:56 +0530

    location issue solved

--- app/src/main/java/com/example/denish/bloodbank/SelectGroup.java
+++ app/src/main/java/com/example/denish/bloodbank/SelectGroup.java
@@ -24,24 +24,13 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResult;
-import com.google.android.gms.location.LocationSettingsStates;
-import com.google.android.gms.location.LocationSettingsStatusCodes;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
-
-import static com.example.denish.bloodbank.MainActivity.RC_TYPE;
 
 public class SelectGroup extends AppCompatActivity
         implements
@@ -81,12 +70,32 @@ public class SelectGroup extends AppCompatActivity
         mMobileno = findViewById(R.id.et_mobileno);
         mNext = findViewById(R.id.btn_next);
 
-//        mGoogleApiClient = new GoogleApiClient.Builder(this)
-//                .addConnectionCallbacks(this)
-//                .addOnConnectionFailedListener(this)
-//                .addApi(LocationServices.API)
-//                .build();
-        getLocationPermission();
+        LocationRequest request = LocationRequest.create();
+        LocationSettingsRequest settingsRequest = new LocationSettingsRequest.Builder()
+                .addLocationRequest(request)
+                .build();
+        LocationServices.getSettingsClient(SelectGroup.this)
+                .checkLocationSettings(settingsRequest)
+                .addOnCompleteListener(new OnCompleteListener() {
+                    public void onComplete(Task task) {
+                        if(task.isSuccessful()) {
+                            // ***REQUEST LAST LOCATION HERE***
+                        } else {
+                            Exception e = task.getException();
+                            if (e instanceof ResolvableApiException) {
+                                // Show the dialog by calling startResolutionForResult(),
+                                // and check the result in onActivityResult()
+                                try {
+                                    ((ResolvableApiException)e).startResolutionForResult(SelectGroup.this, REQUEST_CHECK_SETTINGS);
+                                } catch (IntentSender.SendIntentException e1) {
+                                    e1.printStackTrace();
+                                }
+                            } else {
+                                //Location can not be resolved, inform the user
+                            }
+                        }
+                    }
+                });
 
         mNext.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -162,21 +171,11 @@ public class SelectGroup extends AppCompatActivity
                                 editor.apply();
                                 Log.d(TAG, "onComplete: lat,lon : " + lat + "," + lon);
                             }
-                            else {
-                                Log.d(TAG, "onComplete: inside else where currentlocation is null");
-                                settingsrequest();
-                                //getDeviceLocation();
-//                                Location currentLocation1 = (Location) task.getResult();
-//                                Log.d(TAG, "onComplete: after settingsrequest");
-//                                double lat = currentLocation1.getLatitude();
-//                                double lon = currentLocation1.getLongitude();
+//                            else {
+//                                Log.d(TAG, "onComplete: inside else for Location null");
 //
-//                                SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0); // 0 - for private mode
-//                                SharedPreferences.Editor editor = pref.edit();
-//                                editor.putString("lat", lat + "");
-//                                editor.putString("lon", lon + "");
-//                                editor.apply();
-                            }
+//                                Log.d(TAG, "onComplete: inside else where currentlocation is null");
+//                            }
                         }
                     }
                 });
@@ -240,19 +239,22 @@ public class SelectGroup extends AppCompatActivity
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        //final LocationSettingsStates states = LocationSettingsStates.fromIntent(data);
         switch (requestCode) {
 // Check for the integer request code originally supplied to startResolutionForResult().
             case REQUEST_CHECK_SETTINGS:
                 switch (resultCode) {
                     case Activity.RESULT_OK:
                         //startLocationUpdates();
-                        Log.d(TAG, "onActivityResult: Result Ok");
-                        Toast.makeText(this, "Request : Result Ok", Toast.LENGTH_SHORT).show();
+                        getLocationPermission();
+                        Log.d(TAG, "onActivityResult: Result Ok for GPS");
+                        Toast.makeText(this, "onActivityResult Ok", Toast.LENGTH_SHORT).show();
                         break;
                     case Activity.RESULT_CANCELED:
+                        getLocationPermission();
                         //settingsrequest();//keep asking if imp or do whatever
-                        Log.d(TAG, "onActivityResult: Result cancelled");
-                        Toast.makeText(this, "Request : Result cancelled", Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, "onActivityResult: Result cancelled for GPS");
+                        Toast.makeText(this, "onActivityResult cancelled", Toast.LENGTH_SHORT).show();
                         break;
                 }
                 break;
@@ -260,68 +262,17 @@ public class SelectGroup extends AppCompatActivity
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        settingsrequest();
-    }
-
-    public void settingsrequest()
-    {
-        LocationRequest locationRequest = LocationRequest.create();
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        locationRequest.setInterval(30 * 1000);
-        locationRequest.setFastestInterval(5 * 1000);
-        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
-                .addLocationRequest(locationRequest);
-        builder.setAlwaysShow(true); //this is the key ingredient
-
-        PendingResult<LocationSettingsResult> result =
-                LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient, builder.build());
-        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
-            @Override
-            public void onResult(LocationSettingsResult result) {
-                final Status status = result.getStatus();
-                final LocationSettingsStates state = result.getLocationSettingsStates();
-                switch (status.getStatusCode()) {
-                    case LocationSettingsStatusCodes.SUCCESS:
-                        // All location settings are satisfied. The client can initialize location
-                        // requests here.
-                        getDeviceLocation();
-                        break;
-                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
-                        // Location settings are not satisfied. But could be fixed by showing the user
-                        // a dialog.
-                        try {
-                            // Show the dialog by calling startResolutionForResult(),
-                            // and check the result in onActivityResult().
-                            status.startResolutionForResult(SelectGroup.this, REQUEST_CHECK_SETTINGS);
-                            getDeviceLocation();
-                        } catch (IntentSender.SendIntentException e) {
-                            // Ignore the error.
-                            Toast.makeText(SelectGroup.this, "Error Occured", Toast.LENGTH_SHORT).show();
-                        }
-                        break;
-                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
-                        // Location settings are not satisfied. However, we have no way to fix the
-                        // settings so we won't show the dialog.
-                        break;
-                }
-            }
-        });
-    }
-
-    @Override
     public void onConnected(@Nullable Bundle bundle) {
-        Log.d(TAG, "onConnected: called");
+
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-        Log.d(TAG, "onConnectionSuspended: called");
+
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Log.d(TAG, "onConnectionFailed: called");
+
     }
 }
